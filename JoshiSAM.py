be9dd9c6f5d2f7a657e54e0a9060adca19e369ed
Samuel Medrano <samu100pre@gmail.com>
2021-05-17 13:34:41 -0500

    Mejorando (Ayuda el Score Pitin)

--- JoshiSAM.py
+++ JoshiSAM.py
@@ -19,7 +19,10 @@ ENEMIGO_TAM=20
 ENEMIGO_POS=[random.randint(0,ANCHO-ENEMIGO_TAM),30]
 #ENEMIGO2
 ENEMIGO_TAM2=20
-ENEMIGO_POS2=[random.randint(0,ANCHO-ENEMIGO_TAM),30]
+ENEMIGO_POS2=[random.randint(0,ANCHO-ENEMIGO_TAM2),30]
+#ENEMIGO3
+ENEMIGO_TAM3=20
+ENEMIGO_POS3=[random.randint(0,ANCHO-ENEMIGO_TAM3),30]
 
 #ventana
 ventana= pygame.display.set_mode((ANCHO,ALTO))
@@ -27,7 +30,7 @@ game_over=False
 clock=pygame.time.Clock()
 
 #Funciones
-def detectar_colision(JUGADOR_POS,ENEMIGO_POS):
+def detectar_colision1(JUGADOR_POS,ENEMIGO_POS,):
     jx=JUGADOR_POS[0]
     jy=JUGADOR_POS[1]
     ex=ENEMIGO_POS[0]
@@ -35,8 +38,30 @@ def detectar_colision(JUGADOR_POS,ENEMIGO_POS):
     
     if (ex>jx and ex<(jx+JUGADOR_TAM)) or (jx>=ex and jx<(ex+ENEMIGO_TAM)):
        if (ey>jy and ey<(jy+JUGADOR_TAM)) or (jy>=ex and jy<(ey+ENEMIGO_TAM)):
-           return True
+                return True
     return False
+def detectar_colision2(JUGADOR_POS,ENEMIGO_POS2,):
+    jx=JUGADOR_POS[0]
+    jy=JUGADOR_POS[1]
+    ex=ENEMIGO_POS2[0]
+    ey=ENEMIGO_POS2[1]
+    
+    if (ex>jx and ex<(jx+JUGADOR_TAM)) or (jx>=ex and jx<(ex+ENEMIGO_TAM2)):
+       if (ey>jy and ey<(jy+JUGADOR_TAM)) or (jy>=ex and jy<(ey+ENEMIGO_TAM2)):
+                return True
+    return False
+def detectar_colision3(JUGADOR_POS,ENEMIGO_POS3,):
+    jx=JUGADOR_POS[0]
+    jy=JUGADOR_POS[1]
+    ex=ENEMIGO_POS3[0]
+    ey=ENEMIGO_POS3[1]
+    
+    if (ex>jx and ex<(jx+JUGADOR_TAM)) or (jx>=ex and jx<(ex+ENEMIGO_TAM3)):
+       if (ey>jy and ey<(jy+JUGADOR_TAM)) or (jy>=ex and jy<(ey+ENEMIGO_TAM3)):
+                return True
+    return False
+    
+           
 
 while not game_over:
     for event in pygame.event.get():
@@ -76,18 +101,31 @@ while not game_over:
     if ENEMIGO_POS2[1]>=0 and ENEMIGO_POS2[1]<ALTO:
        ENEMIGO_POS2[1]+=25
     else:
-       ENEMIGO_POS2[0]=random.randint(0,ANCHO-ENEMIGO_TAM)
+       ENEMIGO_POS2[0]=random.randint(0,ANCHO-ENEMIGO_TAM2)
        ENEMIGO_POS2[1]=0
+   #moviemiento enemigo 3
+    if ENEMIGO_POS3[1]>=0 and ENEMIGO_POS3[1]<ALTO:
+       ENEMIGO_POS3[1]+=25
+    else:
+       ENEMIGO_POS3[0]=random.randint(0,ANCHO-ENEMIGO_TAM3)
+       ENEMIGO_POS3[1]=0
 
     #Colisiones
-    if detectar_colision(JUGADOR_POS,ENEMIGO_POS):
+    if detectar_colision1(JUGADOR_POS,ENEMIGO_POS,):
+        game_over=True
+    if detectar_colision2(JUGADOR_POS,ENEMIGO_POS2,):
+        game_over=True
+    if detectar_colision3(JUGADOR_POS,ENEMIGO_POS3,):
         game_over=True
 
     #DIBUJAR ENEMIGO1
     pygame.draw.circle(ventana, RED, (ENEMIGO_POS[0], ENEMIGO_POS[1]), ENEMIGO_TAM, ENEMIGO_TAM)
 
     #DIBUJAR ENEMIGO2
-    pygame.draw.circle(ventana, RED, (ENEMIGO_POS2[0], ENEMIGO_POS2[1]), ENEMIGO_TAM2, ENEMIGO_TAM2)
+    pygame.draw.circle(ventana, WHITE, (ENEMIGO_POS2[0], ENEMIGO_POS2[1]), ENEMIGO_TAM2, ENEMIGO_TAM2)
+
+    #DIBUJAR ENEMIGO3
+    pygame.draw.circle(ventana, BLUE, (ENEMIGO_POS3[0], ENEMIGO_POS3[1]), ENEMIGO_TAM3, ENEMIGO_TAM3)
     
 
     #Dibujar jugador
@@ -95,5 +133,5 @@ while not game_over:
 
 
 
-    clock.tick(40)#Velocidad
+    clock.tick(35)#Velocidad
     pygame.display.update()
