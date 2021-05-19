788ccde553aa9a6f376078855479eca9707aeae5
Samuel Medrano <samu100pre@gmail.com>
2021-05-19 11:11:58 -0500

    cronometro

--- probandotexto.py
+++ probandotexto.py
@@ -1,21 +1,32 @@
 import pygame
-from pygame.locals import *
- 
+
 pygame.init()
- 
-pantalla = pygame.display.set_mode((450,300),0,32)
-pygame.display.set_caption("Modulo de fuentes")
- 
-reloj = pygame.time.Clock()
- 
-fuente = pygame.font.Font(None, 30)
-texto1 = fuente.render("Texto de pruebas", 0, (255, 255, 255))
- 
- 
-while True:
+
+FONT = pygame.font.SysFont("Sans", 20)
+TEXT_COLOR = (0, 0, 0)
+BG_COLOR = (255, 255, 255)
+
+loop = True
+start_time = None
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+while loop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
-    reloj.tick(20)
-    pantalla.blit(texto1, (100,100))
-    pygame.display.update()
+         pygame.quit()
+quit()
+if event.type == pygame.KEYDOWN:
+    if event.key == pygame.K_RETURN:
+        start_time = pygame.time.get_ticks()
+
+screen.fill(BG_COLOR)
+
+if start_time:
+    time_since_enter = pygame.time.get_ticks() - start_time
+    message = "Milliseconds since enter: " + str(time_since_enter)
+    screen.blit(FONT.render(message, True, TEXT_COLOR), (20, 20))
+
+pygame.display.flip()
+clock.tick(60)
+
+pygame.quit()
--- relojito.py
+++ relojito.py
@@ -110,7 +110,86 @@ def detectar_colision5(JUGADOR_POS,ENEMIGO_POS5,):
          
 game_over=False
 running= True
-while not game_over:        
+
+while not game_over:
+    if running:
+        running =False 
+        ventana.fill(BLACK)
+        #Movimiento enemigo 1
+        if ENEMIGO_POS[1]>=0 and ENEMIGO_POS[1]<ALTO:
+            ENEMIGO_POS[1]+=20
+        else:
+            ENEMIGO_POS[0]=random.randint(0,ANCHO-ENEMIGO_TAM)
+            ENEMIGO_POS[1]=0
+   #moviemiento enemigo 2
+        if ENEMIGO_POS2[1]>=0 and ENEMIGO_POS2[1]<ALTO:
+            ENEMIGO_POS2[1]+=25
+        else:
+            ENEMIGO_POS2[0]=random.randint(0,ANCHO-ENEMIGO_TAM2)
+            ENEMIGO_POS2[1]=0
+   #moviemiento enemigo 3
+        if ENEMIGO_POS3[1]>=0 and ENEMIGO_POS3[1]<ALTO:
+            ENEMIGO_POS3[1]+=25
+        else:
+            ENEMIGO_POS3[0]=random.randint(0,ANCHO-ENEMIGO_TAM3)
+            ENEMIGO_POS3[1]=0
+
+    #moviemiento enemigo 4
+        if ENEMIGO_POS4[1]>=0 and ENEMIGO_POS4[1]<ALTO:
+            ENEMIGO_POS4[1]+=25
+        else:
+            ENEMIGO_POS4[0]=random.randint(0,ANCHO-ENEMIGO_TAM4)
+            ENEMIGO_POS4[1]=0
+
+    #moviemiento enemigo 5
+        if ENEMIGO_POS5[1]>=0 and ENEMIGO_POS5[1]<ALTO:
+            ENEMIGO_POS5[1]+=25
+        else:
+            ENEMIGO_POS5[0]=random.randint(0,ANCHO-ENEMIGO_TAM5)
+            ENEMIGO_POS5[1]=0
+
+    #Colisiones
+        if detectar_colision1(JUGADOR_POS,ENEMIGO_POS,):
+            game_over=True
+        if detectar_colision2(JUGADOR_POS,ENEMIGO_POS2,):
+            game_over=True
+        if detectar_colision3(JUGADOR_POS,ENEMIGO_POS3,):
+            game_over=True
+        if detectar_colision4(JUGADOR_POS,ENEMIGO_POS4,):
+            game_over=True
+        if detectar_colision5(JUGADOR_POS,ENEMIGO_POS5,):
+            game_over=True
+
+
+        #DIBUJAR ENEMIGO1
+        pygame.draw.circle(ventana, RED, (ENEMIGO_POS[0], ENEMIGO_POS[1]), ENEMIGO_TAM, ENEMIGO_TAM)
+
+        #DIBUJAR ENEMIGO2
+        pygame.draw.circle(ventana, WHITE, (ENEMIGO_POS2[0], ENEMIGO_POS2[1]), ENEMIGO_TAM2, ENEMIGO_TAM2)
+
+        #DIBUJAR ENEMIGO3
+        pygame.draw.circle(ventana, BLUE, (ENEMIGO_POS3[0], ENEMIGO_POS3[1]), ENEMIGO_TAM3, ENEMIGO_TAM3)
+
+        #DIBUJAR ENEMIGO4
+        pygame.draw.circle(ventana, ORANGE, (ENEMIGO_POS4[0], ENEMIGO_POS4[1]), ENEMIGO_TAM4, ENEMIGO_TAM4)
+        
+        #DIBUJAR ENEMIGO5
+        pygame.draw.circle(ventana, CELESTE, (ENEMIGO_POS5[0], ENEMIGO_POS5[1]), ENEMIGO_TAM5, ENEMIGO_TAM5)
+        
+    
+        #Dibujar jugador
+        pygame.draw.circle(ventana, GREEN, (JUGADOR_POS[0], JUGADOR_POS[1]), JUGADOR_TAM, JUGADOR_TAM)
+
+        
+        segundos=pygame.time.get_ticks()/1000
+        segundos=str(segundos)
+        contador=fuente1.render(segundos,0,(GREEN))
+        ventana.blit(contador, (120,20))
+        ventana.blit(texto1, (300,50))
+        ventana.blit(texto2, (5,20))
+        clock.tick(54)#Velocidad
+        pygame.display.update()
+   
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             sys.exit()
@@ -137,78 +216,4 @@ while not game_over:
            
            
 
-    ventana.fill(BLACK)
-   #Movimiento enemigo 1
-    if ENEMIGO_POS[1]>=0 and ENEMIGO_POS[1]<ALTO:
-       ENEMIGO_POS[1]+=20
-    else:
-       ENEMIGO_POS[0]=random.randint(0,ANCHO-ENEMIGO_TAM)
-       ENEMIGO_POS[1]=0
-   #moviemiento enemigo 2
-    if ENEMIGO_POS2[1]>=0 and ENEMIGO_POS2[1]<ALTO:
-       ENEMIGO_POS2[1]+=25
-    else:
-       ENEMIGO_POS2[0]=random.randint(0,ANCHO-ENEMIGO_TAM2)
-       ENEMIGO_POS2[1]=0
-   #moviemiento enemigo 3
-    if ENEMIGO_POS3[1]>=0 and ENEMIGO_POS3[1]<ALTO:
-       ENEMIGO_POS3[1]+=25
-    else:
-       ENEMIGO_POS3[0]=random.randint(0,ANCHO-ENEMIGO_TAM3)
-       ENEMIGO_POS3[1]=0
-
-    #moviemiento enemigo 4
-    if ENEMIGO_POS4[1]>=0 and ENEMIGO_POS4[1]<ALTO:
-       ENEMIGO_POS4[1]+=25
-    else:
-       ENEMIGO_POS4[0]=random.randint(0,ANCHO-ENEMIGO_TAM4)
-       ENEMIGO_POS4[1]=0
-
-    #moviemiento enemigo 5
-    if ENEMIGO_POS5[1]>=0 and ENEMIGO_POS5[1]<ALTO:
-       ENEMIGO_POS5[1]+=25
-    else:
-       ENEMIGO_POS5[0]=random.randint(0,ANCHO-ENEMIGO_TAM5)
-       ENEMIGO_POS5[1]=0
-
-    #Colisiones
-    if detectar_colision1(JUGADOR_POS,ENEMIGO_POS,):
-        game_over=True
-    if detectar_colision2(JUGADOR_POS,ENEMIGO_POS2,):
-        game_over=True
-    if detectar_colision3(JUGADOR_POS,ENEMIGO_POS3,):
-        game_over=True
-    if detectar_colision4(JUGADOR_POS,ENEMIGO_POS4,):
-        game_over=True
-    if detectar_colision5(JUGADOR_POS,ENEMIGO_POS5,):
-        game_over=True
-
-
-    #DIBUJAR ENEMIGO1
-    pygame.draw.circle(ventana, RED, (ENEMIGO_POS[0], ENEMIGO_POS[1]), ENEMIGO_TAM, ENEMIGO_TAM)
-
-    #DIBUJAR ENEMIGO2
-    pygame.draw.circle(ventana, WHITE, (ENEMIGO_POS2[0], ENEMIGO_POS2[1]), ENEMIGO_TAM2, ENEMIGO_TAM2)
-
-    #DIBUJAR ENEMIGO3
-    pygame.draw.circle(ventana, BLUE, (ENEMIGO_POS3[0], ENEMIGO_POS3[1]), ENEMIGO_TAM3, ENEMIGO_TAM3)
-
-    #DIBUJAR ENEMIGO4
-    pygame.draw.circle(ventana, ORANGE, (ENEMIGO_POS4[0], ENEMIGO_POS4[1]), ENEMIGO_TAM4, ENEMIGO_TAM4)
     
-    #DIBUJAR ENEMIGO5
-    pygame.draw.circle(ventana, CELESTE, (ENEMIGO_POS5[0], ENEMIGO_POS5[1]), ENEMIGO_TAM5, ENEMIGO_TAM5)
-    
-   
-    #Dibujar jugador
-    pygame.draw.circle(ventana, GREEN, (JUGADOR_POS[0], JUGADOR_POS[1]), JUGADOR_TAM, JUGADOR_TAM)
-
-    
-    segundos=pygame.time.get_ticks()/1000
-    segundos=str(segundos)
-    contador=fuente1.render(segundos,0,(GREEN))
-    ventana.blit(contador, (120,20))
-    ventana.blit(texto1, (300,50))
-    ventana.blit(texto2, (5,20))
-    clock.tick(54)#Velocidad
-    pygame.display.update()
